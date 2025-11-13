cc47f0c21db1ca44a93bd10d199276fb8b345c47
gaetaninofresh <larocca.gaetano10@gmail.com>
2025-11-13 19:21:01 +0100

    Progress on rewrite

--- proj/src/decompiler/dd.py
+++ proj/src/decompiler/dd.py
@@ -1,7 +1,11 @@
 import decompile
 
 
+def full_clean(d: decompile.Decompiler):
+    f = d.enum_f()
+    no_xref = d.no_xref_f(f)
+
+
 if __name__ == '__main__':
     d = decompile.Decompiler('./bin')
     f = d.enum_f()
-    s = d.plt_f_sym(f)
